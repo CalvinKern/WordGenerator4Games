3b411c69baf9c64981fb380fc639d5facfead72d
Calvin Kern <calvinmkern@gmail.com>
2018-03-23 16:27:06 -0600

    Project creation + initial layout

---
+++ app/src/main/java/com/seakernel/android/wordgenerator4games/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.seakernel.android.wordgenerator4games
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_controls)
+    }
+}
